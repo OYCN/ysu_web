7c7d4887686b6471fe9dfd58d69c5aadcc6c1556
oPluss <961072909@qq.com>
2019-08-31 16:03:26 +0800

    基本结构

--- web/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebConfig(AppConfig):
-    name = 'web'
-    verbose_name = u'网站'
--- web_permission/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebPermissionConfig(AppConfig):
-    name = 'web_permission'
-    verbose_name = u'权限管理'
